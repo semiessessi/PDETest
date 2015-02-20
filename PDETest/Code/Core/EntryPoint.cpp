cacb0201a502174e27cfd25e00b6157769f848be
Semi Essessi <semi@cranium-software.co.uk>
2015-02-20 00:05:55 +0000

    add uranus tests

--- PDETest/Code/Core/EntryPoint.cpp
+++ PDETest/Code/Core/EntryPoint.cpp
@@ -3,15 +3,18 @@
 #include "Test/Test.h"
 #include "Test/Ephemerides/Test_JupiterPosition.h"
 #include "Test/Ephemerides/Test_SaturnPosition.h"
+#include "Test/Ephemerides/Test_UranusPosition.h"
 
 int main( const char* const* const /*pszArguments*/, const int /*iArgumentCount*/ )
 {
 	// register a bunch of tests
 	Test_JupiterPosition xTest_JupiterPosition;
 	Test_SaturnPosition xTest_SaturnPosition;
+    Test_UranusPosition xTest_UranusPosition;
 
 	Test::Register( &xTest_JupiterPosition );
 	Test::Register( &xTest_SaturnPosition );
+    Test::Register( &xTest_UranusPosition );
 
 	return Test::RunAllTests() ? 0 : -1;
 }
---
+++ PDETest/Code/Test/Ephemerides/Test_UranusPosition.h
@@ -0,0 +1,17 @@
+#ifndef TEST_URANUS_POSITION_H
+#define TEST_URANUS_POSITION_H
+
+#include "Test/Test.h"
+
+class Test_UranusPosition
+: public Test
+{
+
+public:
+
+    virtual bool Run();
+    virtual const char* GetName() const { return "Uranus Position"; }
+
+};
+
+#endif
